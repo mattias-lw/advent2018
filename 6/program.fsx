0fc96057393c60a42a43a5c08c3f5f7cf0f4fe90
Mattias Tidlund <mattias.tidlund@learningwell.se>
2018-12-06 22:06:48 +0100

    Day 6, both problems, very slow

--- 6/program.fsx
+++ 6/program.fsx
@@ -1,8 +1,5 @@
 open System.IO
-open System
 open System.Text.RegularExpressions
-open System.Runtime.InteropServices
-open System.Linq
 
 let data = File.ReadAllLines("data.txt") 
           |> Seq.map (fun l -> Regex.Matches(l,"\\d+") |> Seq.map (fun m -> m.Groups.[0].Captures.[0].Value))
@@ -21,19 +18,28 @@ let dists = fun d dm c ->
              |Some (odist,(ox,oy)),d,dist when dist < odist -> (dist,d)
              |Some (odist,(ox,oy)),_,dist when dist = odist -> (dist,(2147483647,2147483647))
              |Some o,_,_ -> o) dm
-let calc2 = fun dim data->
-  
+let calc = fun dim data->
   let cords = seq {for x in -dim.. (dim-1) do
                     for y in -dim .. (dim-1) do
                       yield (x,y)
   }
-
   Seq.fold (fun m d -> Seq.fold (dists d) m cords) Map.empty data |> Map.toSeq |> Seq.map snd |> Seq.groupBy snd |> Seq.map (fun e -> (Seq.length (snd e), (fst e))) |>Set.ofSeq
 
-let result1 = calc2 600 data
-printfn "Stage 1 done"
-let result2 = calc2 702 data
-printfn "Stage 2 done"
+let distsAcc = fun d dm c ->  
+  Map.add c (match (Map.tryFind c dm),d,(distance c d) with
+             |None,d , dist -> dist
+             |Some odist,d,dist when (dist + odist) < 10000 -> dist + odist
+             |Some _,_,_ -> 999999) dm
+
+let calc2 = fun dim data-> 
+  let cords = seq {for x in -dim.. (dim-1) do
+                    for y in -dim .. (dim-1) do
+                      yield (x,y)}
+  Seq.fold (fun m d -> Seq.fold (distsAcc d) m cords) Map.empty data |> Map.toSeq |> Seq.map snd |> Seq.filter (fun i -> i < 10000) |> Seq.length
+
+let result1 = calc 600 data
+let result2 = calc 602 data
 let result = Set.intersect result1 result2  |> Seq.maxBy fst
-printfn "Stage 3 done"
-printfn "%d %d %d" (fst result) (fst (snd result)) (snd (snd result))
+printfn "%d" (fst result)
+
+printfn "%d" (calc2 400 data)
