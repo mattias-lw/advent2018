68bb65f689f11459fb1b8b75ce72601f721603da
Mattias Tidlund <mattias.tidlund@learningwell.se>
2018-12-06 20:12:47 +0100

    Update 6-1, functional but slow

--- 6/program.fsx
+++ 6/program.fsx
@@ -1,6 +1,8 @@
 open System.IO
 open System
 open System.Text.RegularExpressions
+open System.Runtime.InteropServices
+open System.Linq
 
 let data = File.ReadAllLines("data.txt") 
           |> Seq.map (fun l -> Regex.Matches(l,"\\d+") |> Seq.map (fun m -> m.Groups.[0].Captures.[0].Value))
@@ -12,36 +14,26 @@ let distance = fun a b ->
   match a,b with
   | (x1,y1),(x2,y2) -> (abs (x1 - x2)) + (abs (y1 - y2))
 
-//let printfield = fun dim x_cords y_cords field ->  
-//  for x in x_cords do
-//    for y in y_cords do
-//      match field.[x+dim + 2 * dim * (y+dim)] with
-//      | (dist,(ox,oy)) ->  printf "(%2d, (%2d,%2d) " dist ox oy
-//    printfn ""
- 
-let calc = fun dim ->
 
-  let x_cords = seq {-dim .. dim-1}
-  let y_cords = seq {-dim .. dim-1} 
+let dists = fun d dm c ->  
+  Map.add c (match (Map.tryFind c dm),d,(distance c d) with
+             |None,d , dist -> (dist,d)
+             |Some (odist,(ox,oy)),d,dist when dist < odist -> (dist,d)
+             |Some (odist,(ox,oy)),_,dist when dist = odist -> (dist,(2147483647,2147483647))
+             |Some o,_,_ -> o) dm
+let calc2 = fun dim data->
+  
+  let cords = seq {for x in -dim.. (dim-1) do
+                    for y in -dim .. (dim-1) do
+                      yield (x,y)
+  }
 
-  let field = Array.create (2*dim*2*dim) (2147483647,(0,0))
+  Seq.fold (fun m d -> Seq.fold (dists d) m cords) Map.empty data |> Map.toSeq |> Seq.map snd |> Seq.groupBy snd |> Seq.map (fun e -> (Seq.length (snd e), (fst e))) |>Set.ofSeq
 
-  let mark = fun c ->
-    for x in x_cords do
-      for y in y_cords do
-        let old = field.[x+dim + 2 * dim * (y+dim)]
-        Array.set field (x+dim + 2 * dim * (y+dim)) (match old,c,(distance c (x,y)) with
-                                                     |(odist,(ox,oy)),c,dist when dist < odist -> (dist,c)
-                                                     |(odist,(ox,oy)),c,dist when dist = odist -> (dist,(2147483647,2147483647))
-                                                     |o,_,_ -> o
-        )
-
-  data |> Seq.iter mark
-
-  field |> Seq.groupBy snd |> Seq.map (fun e -> (Seq.length (snd e), (fst e)))
-
-let result1 = calc 601
-let result2 = calc 500
-let result = Seq.filter (fun e -> Seq.contains e result1) result2 |> Seq.maxBy fst
-
+let result1 = calc2 600 data
+printfn "Stage 1 done"
+let result2 = calc2 702 data
+printfn "Stage 2 done"
+let result = Set.intersect result1 result2  |> Seq.maxBy fst
+printfn "Stage 3 done"
 printfn "%d %d %d" (fst result) (fst (snd result)) (snd (snd result))
