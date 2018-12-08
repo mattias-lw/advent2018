ae3d7c05573221c2023e1ec9dbfd02fa601807f0
Mattias Tidlund <mattias.tidlund@learningwell.se>
2018-12-08 19:18:09 +0100

    day 8, task 1 without mutable state

--- 8/program.fsx
+++ 8/program.fsx
@@ -3,23 +3,17 @@ open System.Text.RegularExpressions
 
 let data = Regex.Split(File.ReadAllText("data.txt")," ") |> Seq.map int |> Seq.toList
 
-let sum = ref 0;
-let skipAndSum n list =
-  let localSum = (List.take n list |> List.sum)
-  sum := !sum + localSum
-  List.skip n list
 
-
-let rec sumFirstTree input n =
+let rec sumFirstTree (input:int list * int) (n:int) : int list * int =
   match input with
-  | [] -> []
-  | [a] -> []
-  | 0::nrMeta::rest -> skipAndSum nrMeta rest |> Seq.toList
-  | nrChilds :: nrMeta :: rest -> skipAndSum nrMeta (sumSubs nrChilds (rest |> Seq.toList))
-and sumSubs nrChilds input = 
+  | ([],_)  -> ([],0)
+  | ([a],_) -> ([],0)
+  | (0::nrMeta::rest,sum) -> (List.skip nrMeta rest |> Seq.toList,sum + (List.take nrMeta rest |> List.sum))
+  | (nrChilds :: nrMeta :: rest,sum) -> match sumSubs nrChilds (rest |> Seq.toList, 0) with 
+                                        | (newRest, subSum) -> (List.skip nrMeta newRest, sum + subSum + (List.take nrMeta newRest |> List.sum))
+and sumSubs (nrChilds:int) (input:int list * int) : int list * int= 
   let childSeq = seq {1..nrChilds}
   Seq.fold sumFirstTree input childSeq
   
 
-sumFirstTree data 0
-printfn "%d" !sum
+sumFirstTree (data,0) 0
